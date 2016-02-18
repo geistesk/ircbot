94fa432a727329da77871595ec9ba7ba340c5ba2
geistesk <github.jyf300hj@0x21.biz>
2016-02-18 20:51:12 +0100

    Freifunka-Module is now greeting

--- lib/freifunka_greeting_handler.ex
+++ lib/freifunka_greeting_handler.ex
@@ -11,7 +11,7 @@ defmodule FreifunkaGreetingHandler do
     {:ok, client}
   end
 
-  def handle_info({:joined, channel, user}, client) do
+  def handle_info({:joined, _channel, user}, client) do
     # Asking who the new user is…
     # Response will be consumed in the next function
 
@@ -21,11 +21,18 @@ defmodule FreifunkaGreetingHandler do
 
   def handle_info({:unrecognized, "352", %IrcMessage{:cmd => "352", :args => resp}}, client) do
     # 352 is the magic number for an /who-Response which data
-    # The {fifth,third} element in the resulting string is the {nick,host}name
+    # The {5th,3rd,1st} element in the resulting string is the {nick,host,channel}name
 
-    {nick, host} = { Enum.at(resp, 5, ""), Enum.at(resp, 3, "") }
-    debug "#{nick} @ #{host}"
-    # TODO
+    {nick, host, channel} = {
+      Enum.at(resp, 5, ""), Enum.at(resp, 3, ""), Enum.at(resp, 1) }
+
+    # If the user from the webchat than let's grett :3
+    if nick != "" and host == "2001:4dd0:fc15:cafe:208:54ff:fe55:1498" do
+      ["Hej #{nick}!",
+       "Es freut uns, dass du es über den Webchat zu uns geschafft hast.",
+       "Falls du Fragen hast, stelle sie einfach. Bitte bedenke, dass es aber etwas dauer kann, bis wer antwortet…"]
+      |> Enum.each(fn txt -> ExIrc.Client.msg(client, :privmsg, channel, txt) end)
+    end
 
     {:noreply, client}
   end
