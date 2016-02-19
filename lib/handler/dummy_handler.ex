907e7cf83a9ba2ac95abe808fc7adef0114591ed
geistesk <github.jyf300hj@0x21.biz>
2016-02-19 12:08:06 +0100

    Commented DummyHandler out to prevent warnings

--- lib/handler/dummy_handler.ex
+++ lib/handler/dummy_handler.ex
@@ -1,22 +1,22 @@
-defmodule DummyHandler do
-  @moduledoc """
-  This is an example event handler which does nothing :3
-  """
-  def start_link(client) do
-    GenServer.start_link(__MODULE__, [client])
-  end
-
-  def init([client]) do
-    ExIrc.Client.add_handler client, self
-    {:ok, client}
-  end
-
-  # Catch-all for messages you don't care about
-  def handle_info(_msg, state) do
-    {:noreply, state}
-  end
-
-  defp debug(msg) do
-    IO.puts IO.ANSI.yellow() <> msg <> IO.ANSI.reset()
-  end
-end
+# defmodule DummyHandler do
+#   @moduledoc """
+#   This is an example event handler which does nothing :3
+#   """
+#   def start_link(client) do
+#     GenServer.start_link(__MODULE__, [client])
+#   end
+#
+#   def init([client]) do
+#     ExIrc.Client.add_handler client, self
+#     {:ok, client}
+#   end
+#
+#   # Catch-all for messages you don't care about
+#   def handle_info(_msg, state) do
+#     {:noreply, state}
+#   end
+#
+#   defp debug(msg) do
+#     IO.puts IO.ANSI.yellow() <> msg <> IO.ANSI.reset()
+#   end
+# end
