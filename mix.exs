24d80f815d8d48b07a94f3782743a5190e1d356b
geistesk <github.jyf300hj@0x21.biz>
2016-02-22 14:14:12 +0100

    Automatically generated releases by exrm

--- mix.exs
+++ mix.exs
@@ -7,19 +7,23 @@ defmodule Ircbot.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     # escript: [main_module: HsmrIrc],
      deps: deps]
   end
 
   def application do
     [
       mod: {HsmrIrc, []},
-      applications: [:exirc, :httpoison, :logger]
+      applications: [:exirc, :httpoison, :spaceapi, :poison]
     ]
   end
 
   defp deps do
     [{:exirc, "~> 0.9.2"},
      {:spaceapi, "~> 0.1.2"},
-     {:httpoison, "~> 0.8.0"}]
+     {:httpoison, "~> 0.8.0"},
+     {:exrm, "~> 1.0.0-rc8"},
+     {:cf, "~> 0.2.1", override: true}, # remove this later
+     {:erlware_commons, github: "erlware/erlware_commons", override: true}] # remove this later
   end
 end
